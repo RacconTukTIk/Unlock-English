98e44370ef1e4811e59da909bd9ddcfa226cce81
Даниил Синельниковя <dsinelnikov857@gmail.com>
2024-11-08 20:01:31 +0300

    Add project
    Add project and registration window

---
+++ Код/app/src/main/java/com/example/unlockenglish/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.app
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.unlockenglish.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val userLogin: EditText = findViewById(R.id.user_login)
+        val userEmail: EditText = findViewById(R.id.user_email)
+        val userPassword: EditText = findViewById(R.id.user_password)
+        val button: Button = findViewById(R.id.button_reg)
+
+        button.setOnClickListener {
+            val login = userLogin.text.toString().trim()
+            val email = userEmail.text.toString().trim()
+            val password = userPassword.text.toString().trim()
+
+
+            if (login == "" || email == "" || password == "")
+                Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_LONG).show()
+            else {
+                val user = User(login, email, password)
+            }
+        }
+    }
+}
---
+++ Код/app/src/main/java/com/example/unlockenglish/User.kt
@@ -0,0 +1,5 @@
+package com.example.app
+
+class User(val login:String,val email:String,val password:String) {
+
+}
