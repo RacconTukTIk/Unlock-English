579415a3cea34bc3aa236f9f6bb6f5ac176d3d03
Galdin Ivan <146315048+RacconTukTIk@users.noreply.github.com>
2025-03-14 19:05:49 +0300

    update mistakes window

--- app/src/main/java/com/example/application/MenuFragment.kt
+++ app/src/main/java/com/example/application/MenuFragment.kt
@@ -9,6 +9,9 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import com.example.application.R
+import com.example.application.MistakesActivity
+import com.example.dicti.DictTechActivity
+
 
 class MenuFragment : Fragment() {
     override fun onCreateView(
@@ -17,16 +20,20 @@ class MenuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.activity_menu, container, false)
+
         val buttonThemes: Button = view.findViewById(R.id.button_themes)
         buttonThemes.setOnClickListener {
-            val intent = Intent(requireActivity(),ThemesActivity::class.java)
+            val intent = Intent(requireActivity(), ThemesActivity::class.java)
+            startActivity(intent)
+        }
+        val buttonError: Button = view.findViewById(R.id.button_mistakes)
+        buttonError.setOnClickListener {
+            val intent = Intent(requireActivity(), MistakesActivity::class.java)
             startActivity(intent)
         }
         return view
     }
-
-
-
+    
     companion object {
         @JvmStatic
         fun newInstance() = MenuFragment()
